78eb5928d0d7fd365ecb5b2b437f13aa67c32622
Maikol Guzman <mikeguzman@gmail.com>
2020-05-18 01:05:44 -0600

    Port updated
    Signed-off-by: Maikol Guzman <mikeguzman@gmail.com>

--- src/main/java/cr/una/full/backend/BackendServiceApplication.java
+++ src/main/java/cr/una/full/backend/BackendServiceApplication.java
@@ -23,11 +23,17 @@ package cr.una.full.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class BackendServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendServiceApplication.class, args);
+		SpringApplication app = new SpringApplication(BackendServiceApplication.class);
+		app.setDefaultProperties(Collections
+				.singletonMap("server.port", "8083"));
+		app.run(args);
+
 	}
 
 }
